c0d54141be80a252afcf74b0c6c451ecec247897
Abraham Kimani <abrahamkuria9@gmail.com>
2026-06-09 09:59:35 +0300

    Initial Commit

---
+++ mkopoplanUITests/mkopoplanUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  mkopoplanUITestsLaunchTests.swift
+//  mkopoplanUITests
+//
+//  Created by Abraham Kimani on 09/06/2026.
+//
+
+import XCTest
+
+final class mkopoplanUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
